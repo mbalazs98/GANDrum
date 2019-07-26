cdee1e02d5f7531f3399230b7e1f66931a581ce5
mbalazs98 <49280623+mbalazs98@users.noreply.github.com>
2019-07-26 16:01:41 +0200

    Update model.py

--- model.py
+++ model.py
@@ -26,25 +26,35 @@ class GANDrum(object):
 		
 	def make_generator_model():
 		model=tf.keras.Sequential()
-		model.add(layers.Dense(7*7*256,use_bias=False,input_shape=(100,)))
+		
+		
+		model.add(layers.Dense(units=1024,input_shape=(100,)))
 		model.add(layers.BatchNormalization())
-		model.add(layers.LeakyReLU())
-
-		model.add(layers.Reshape((7,7,256)))
-		assert model.output_shape==(None,7,7,256)
-
-		model.add(layers.Conv2DTranspose(128,(5,5),strides=(1,1),padding='same',use_bias=False))
-		assert model.output_shape==(None,7,7,128)
+		model.add(layers.softmax())
+		
+		models.add(Dense(units=512))
 		model.add(layers.BatchNormalization())
-		model.add(layers.LeakyReLU())
+		model.add(layers.softmax())
 
-		model.add(layers.Conv2DTranspose(64,(5,5),strides=(2,2),padding='same',use_bias=False))
-		assert model.output_shape==(None,14,14,64)
+
+		model.add(layers.Reshape((1,2)))
+
+
+		model.add(layers.Conv2DTranspose(filter=1,kernel_size=2,strides=2))
 		model.add(layers.BatchNormalization())
-		model.add(layers.LeakyReLU())
+		model.add(layers.softmax())
+		
+		model.add(layers.Conv2DTranspose(filter=1,kernel_size=2,strides=2))
+		model.add(layers.BatchNormalization())
+		model.add(layers.softmax())
+		
+		model.add(layers.Conv2DTranspose(filter=1,kernel_size=2,strides=2))
+		model.add(layers.BatchNormalization())
+		model.add(layers.softmax())
 
-		model.add(layers.Conv2DTranspose(1,(5,5),strides=(2,2),padding='same',use_bias=False,activation='tanh'))
-		assert model.output_shape==(None,28,28,1)
+		model.add(layers.Conv2DTranspose(filter=128,kernel_size=1,strides=1,activation='tanh')
+		model.add(layers.BatchNormalization())
+		model.add(layers.softmax())
 
 		return model
 	
