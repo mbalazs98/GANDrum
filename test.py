f65b9be075823e6e766d94a64b267ef1d84fb2a0
mbalazs98 <49280623+mbalazs98@users.noreply.github.com>
2019-08-06 17:16:51 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,36 +4,52 @@ import numpy as np
 import os
 
 max=0
-
-entries=os.listdir(r'D:/midi_d/Electronic/')
+entries=os.listdir(r'E:/GitHub/GANDrum/midi_d/Electronic/')
+i=0
 for entry in entries:
-	x=pp.parse(entry)
-	pp.save(r'D:\midi_d\your_training_data',x,compressed=False)
-	data=np.load(r'D:\midi_d\your_training_data.npz')
-	mtx=sparse.csc_matrix((data['pianoroll_0_csc_data'], data['pianoroll_0_csc_indices'], data['pianoroll_0_csc_indptr'])).todense()
-	if max<mtx.shape[0]:
-		max=mtx.shape[0]
-		print(max)
+	i=i+1
+	try:
+		x=pp.parse(r'E:/GitHub/GANDrum/midi_d/Electronic/'+entry)
+		pp.save(r'E:/GitHub/GANDrum/midi_d/your_training_data',x,compressed=False)
+		data=np.load(r'E:/GitHub/GANDrum/midi_d/your_training_data.npz')
+		mtx=sparse.csc_matrix((data['pianoroll_0_csc_data'], data['pianoroll_0_csc_indices'], data['pianoroll_0_csc_indptr'])).todense()
+		if max<mtx.shape[0]:
+			max=mtx.shape[0]
+			print(max)
+			print(i)
+	except RuntimeWarning:
+		pass
 
-entries=os.listdir(r'D:/midi_d/FunkRnB/')
+i=0
+entries=os.listdir(r'E:/GitHub/GANDrum/midi_d/FunkRnB/')
 for entry in entries:
-	x=pp.parse(entry)
-	pp.save(r'D:\midi_d\your_training_data',x,compressed=False)
-	data=np.load(r'D:\midi_d\your_training_data.npz')
-	mtx=sparse.csc_matrix((data['pianoroll_0_csc_data'], data['pianoroll_0_csc_indices'], data['pianoroll_0_csc_indptr'])).todense()
-	if max<mtx.shape[0]:
-		max=mtx.shape[0]
-		print(max)
-
-entries=os.listdir(r'D:/midi_d/World/')
+	i=i+1
+	try:
+		x=pp.parse(r'E:/GitHub/GANDrum/midi_d/FunkRnB/'+entry)
+		pp.save(r'E:/GitHub/GANDrum/midi_d/your_training_data',x,compressed=False)
+		data=np.load(r'E:/GitHub/GANDrum/midi_d/your_training_data.npz')
+		mtx=sparse.csc_matrix((data['pianoroll_0_csc_data'], data['pianoroll_0_csc_indices'], data['pianoroll_0_csc_indptr'])).todense()
+		if max<mtx.shape[0]:
+			max=mtx.shape[0]
+			print(max)
+			print(i)
+	except RuntimeWarning:
+		pass
+i=0
+entries=os.listdir(r'E:/GitHub/GANDrum/midi_d/World/')
 for entry in entries:
-	x=pp.parse(entry)
-	pp.save(r'D:\midi_d\your_training_data',x,compressed=False)
-	data=np.load(r'D:\midi_d\your_training_data.npz')
-	mtx=sparse.csc_matrix((data['pianoroll_0_csc_data'], data['pianoroll_0_csc_indices'], data['pianoroll_0_csc_indptr'])).todense()
-	if max<mtx.shape[0]:
-		max=mtx.shape[0]
-		print(max)
+	i=i+1
+	try:
+		x=pp.parse(r'E:/GitHub/GANDrum/midi_d/World/'+entry)
+		pp.save(r'E:/GitHub/GANDrum/midi_d/your_training_data',x,compressed=False)
+		data=np.load(r'E:/GitHub/GANDrum/midi_d/your_training_data.npz')
+		mtx=sparse.csc_matrix((data['pianoroll_0_csc_data'], data['pianoroll_0_csc_indices'], data['pianoroll_0_csc_indptr'])).todense()
+		if max<mtx.shape[0]:
+			max=mtx.shape[0]
+			print(max)
+			print(i)
+	except RuntimeWarning:
+		pass
 		
 
 
