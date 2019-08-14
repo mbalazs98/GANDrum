6d8189bfb2e233f16aaf9ed5131f55fd1a9c794d
mbalazs98 <49280623+mbalazs98@users.noreply.github.com>
2019-08-14 23:09:50 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,25 +1,22 @@
 from scipy import sparse
 import pypianoroll as pp
 import numpy as np
+import os
 
-entries=os.listdir(r'E:/GitHub/GANDrum/midi_d/Electronic/')
-j=0
+entries=os.listdir(r'D:\GANDrum\midi_d\World')
+j=1
 for entry in entries:
-	j=j+1
-
-	x=pp.parse(r'E:/GitHub/GANDrum/midi_d/Electronic/(1).mid')
-	pp.save(r'E:/GitHub/GANDrum/your_training_data',x,compressed=False)
-	data=np.load(r'E:/GitHub/GANDrum/your_training_data.npz')
-	mtx=sparse.csc_matrix((data['pianoroll_0_csc_data'], data['pianoroll_0_csc_indices'], data['pianoroll_0_csc_indptr'])).todense()
-	tempo=data['tempo']
-	downbeat=data['downbeat']
-
-
-	for i in range (0,int(mtx.shape[0]/96)):
-		
-		curr_mtx=mtx[i*96:(i+1)*96,:]
-		track=[pp.Track(pianoroll=curr_mtx,is_drum=True)]
-		multitrack=pp.Multitrack(tracks=track,tempo=tempo,downbeat=downbeat)
-		pp.write(multitrack,r'E:/GitHub/GANDrum/{}.mid'.format(i))
 	
 
+	data=pp.parse(r'D:/GANDrum/midi_d/World/'+entry)
+	tempo=data.tempo
+	downbeat=data.downbeat
+
+
+	for i in range (0,int(data.tracks[0].pianoroll.shape[0]/96)):
+		
+		curr_mtx=data.tracks[0].pianoroll[i*96:(i+1)*96,:]
+		track=[pp.Track(pianoroll=curr_mtx,is_drum=True)]
+		multitrack=pp.Multitrack(tracks=track,tempo=tempo,downbeat=downbeat)
+		pp.write(multitrack,r'D:\GANDrum\midi_d_processed\World{}.mid'.format(j))
+		j=j+1
