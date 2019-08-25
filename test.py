76057fdac735ddd4a7c86e9cc5500e319d6ffd31
mbalazs98 <49280623+mbalazs98@users.noreply.github.com>
2019-08-25 18:16:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
+import os
+import pypianoroll as pp
+import numpy as np
 import tensorflow as tf
-(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
-print(train_images)
+from tensorflow.keras import layers
+				
+				
+model=tf.keras.Sequential()
+			
+model.add(layers.Conv2D(filters=128,kernel_size=1,strides=1,input_shape=[8,12,128]))
+model.add(layers.LeakyReLU())
+
+model.add(layers.Conv2D(filters=1,kernel_size=2,strides=2))
+model.add(layers.LeakyReLU())
+			
+model.add(layers.Conv2D(filters=1,kernel_size=2,strides=2))
+model.add(layers.LeakyReLU())
+			
+model.add(layers.Conv2D(filters=1,kernel_size=2,strides=2))
+model.add(layers.LeakyReLU())
+
+model.add(layers.Flatten())
+model.add(layers.Dense(1))
+model.summary()
